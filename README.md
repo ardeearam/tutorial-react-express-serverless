65e6aa985ae42222cfdbbbee850696683a3268f1
Ardee Aram <ardee.aram@gmail.com>
2021-01-19 00:51:34 +0000

    Added README + code cleanup

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # tutorial-react-express-serverless
 Create a serverless fortune cookie app using React, ExpressJS, and AWS!
 
+This sample code demonstrates how you can create a React frontend, and connect it to an ExpressJS app deployed as an AWS Lambda function. 
+The expressJS app is a proxy that fetches the fortune messages from https://djnhavesfyhcf.cloudfront.net/.
+
 Check out live demo here: https://djnhavesfyhcf.cloudfront.net/
--- my-express/index.js
+++ my-express/index.js
@@ -5,21 +5,10 @@ const app = express();
 const axios = require('axios'); 
 
 app.use(cors({
-  origin: '*'
+  origin: 'djnhavesfyhcf.cloudfront.net'
 }));
 
 app.get('/', async function (req, res) {
-  /*
-  const fortunes = [
-    'Lead by example, not by words alone.',
-    "Pay attention to your family. Don't take them for granted.",
-    'Just have fun.',
-    'Love is right around the corner.',
-    'Tomorrow is another day.'
-    
-  ];
-  */
-    //res.send('Baka');
     const { data }  = await axios.get('http://yerkee.com/api/fortune');
     const { fortune } = data;
     res.send(fortune);
